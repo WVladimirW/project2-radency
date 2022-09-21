473b37f0c55fa21719cb1a227e24f9bc9c72ea66
Vladimir Vasyliev <wvladimirw0502@gmail.com>
2022-09-21 21:08:48 +0300

    Updates

--- src/store/reducers/rootReducers.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import { reducer } from './Reducer'
-
-export const rootReducers = combineReducers({
-   reducer
-})
--- src/store/state.ts
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import { reducer, initialState } from './reducers/Reducer'
-
-export const store = createStore(reducer, initialState)
-
-export default store
